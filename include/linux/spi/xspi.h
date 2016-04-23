3e8c788a563e53b345aba5957744179a8efab068
Jayesh <jayesh.babu@windriver.com>
2016-04-23 18:22:43 +0900

    0001-xspi-sram-access

--- include/linux/spi/xspi.h
+++ include/linux/spi/xspi.h
@@ -4,22 +4,24 @@
  * Copyright (C) 2014 Wind River Systems Inc.
  *
  */
-
+/*
+ * change type 
+ */
 #ifndef	XSPI_H_
 #define	XSPI_H_
 
-typedef	struct _XSPILIB_PARAM {
-	int		unit;
-	unsigned char	offset;
-	void		*buf;
-	unsigned long	size;
-}XSPILIB_PARAM, *PXSPILIB_PARAM;
+typedef	struct _SPILIB_PARAM {
+	unsigned int	unit;	/* Unit # */
+	unsigned int	offset;	/* write/read start address */
+	void		*buf;	/* write/read buffer address */
+	size_t		size;	/* write/read byte size */ 
+}SPILIB_PARAM, *PSPILIB_PARAM;
 
-#define	XSPI_UNIT1	0;
-#define	XSPI_UNIT2	1;
-#define	XSPI_UNIT3	2;
+#define	SPI_UNIT1	0	/* CPLD */
+#define	SPI_UNIT2	1	/* SRAM	*/
+#define	SPI_UNIT3	2	/* MRAM/FRAM */
 
-extern	int	xspi_read(XSPILIB_PARAM *spiParam);
-extern	int	xspi_write(XSPILIB_PARAM *spiParam);
+extern	int	xspi_read(SPILIB_PARAM *spiParam);
+extern	int	xspi_write(SPILIB_PARAM *spiParam);
 
 #endif	/* XSPI_ */
